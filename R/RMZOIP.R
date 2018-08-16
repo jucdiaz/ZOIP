3923fd8b640600ccb70633c79bcee577ca55eef5
jucdiaz <jucdiazza@unal.edu.co>
2018-08-16 00:02:31 -0500

    Hago cambios para probar ajuste sin inflaciones3

--- R/RMZOIP.R
+++ R/RMZOIP.R
@@ -134,7 +134,7 @@ RM.ZOIP<-function(formula.mu,formula.sigma=~1,formula.p0=~1,formula.p1=~1,data,l
   opt$par2[opt$par>1.000000e-4]<-opt$par[opt$par>1.000000e-4]
   opt$par2[opt$par<=1.000000e-4]<-1.000000e-4
 
-  HM<-numDeriv::hessian(func=ll.ZOIP2,x=opt$par,y=y,method='Richardson', X.mu=X.mu, X.sigma=X.sigma,X.p0=X.p0,X.p1=X.p1,link=link,family=family)
+  HM<-numDeriv::hessian(func=ll.ZOIP2,x=opt$par2,y=y,method='Richardson', X.mu=X.mu, X.sigma=X.sigma,X.p0=X.p0,X.p1=X.p1,link=link,family=family)
 
   i=1
   b=0
